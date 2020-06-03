167fdb6d9b278b9cce409e6feb2873b1d2b6b883
john_greaves <i.gorpynych-raduzhen@kyivstar.net>
2020-06-03 17:41:33 +0300

    4 course_3

---
+++ 4 курс/Орлова/KP/CourseWork/src/js/main.js
@@ -0,0 +1,11 @@
+
+var MIN_NODES = 28;
+var REGIONS_NUMBER = 4;
+var MIN_NODES_IN_REGION = 7;
+var SATELLITE = 2;
+var AVERAGE_RANG = 4;
+var WEIGHT_ARRAY = [1, 2, 3, 4, 5, 7, 10, 12, 14, 17, 19, 21];
+
+var IS_REGIONAL = true;
+
+
